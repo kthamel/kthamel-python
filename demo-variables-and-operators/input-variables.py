3d8bd1f74a8a2b25674462b5de41589ae551d591
Kushan Thamel <kushan.thamel@gmail.com>
2024-02-12 21:14:07 +0000

    simple content was added

---
+++ demo-variables-and-operators/input-variables.py
@@ -0,0 +1,7 @@
+print("Enter your Name: ", end='') # Using end='' we can get the input into the same line
+name = input()
+
+print("Enter your age:",  end='')
+age = input()
+
+print("Hello", name, "You are", age, "years old")
---
+++ demo-variables-and-operators/operators.py
@@ -0,0 +1,32 @@
+age = 20
+is_young = age < 18 # Want to check the age and identify its True or False. 
+
+print(is_young) 
+##############################
+speed = 80
+is_limit = speed == 80 # Double equal sign means, is equal. Single equal means assign a value to variable.
+
+print(is_limit)
+##############################
+a = True
+b = False
+
+c = not a # Not operator. This gives the oposite boleean value.
+print(c)
+##############################
+x = True
+y = False
+
+d = x ^ y # This operator is using to compare values are not equal.
+print(d)
+##############################
+i = "apple"
+
+print(type(i)) # Type operator is using to fetch the data type
+##############################
+
+aa = "10"
+bb = 20
+
+cc = bb/int(aa) # Using int() we can convert "String" into "Integer"
+print(cc)
