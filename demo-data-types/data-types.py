bb1c014eb703233b600b9724c9940c9d0cdfdbc8
Kushan Thamel <kushan.thamel@gmail.com>
2024-02-12 00:07:54 +0000

    simple operation python files were added

---
+++ demo-data-types/data-types.py
@@ -0,0 +1,6 @@
+print(123) # Integer data type
+print(123.123) # Floating data type
+print("Hello abc 123")  # String data type
+print(True) # Boolean data type
+print(False) # Boolean data type 
+print(1+2j) # Complex numbers data type
---
+++ demo-escape-characters/escape-characters.py
@@ -0,0 +1,13 @@
+print("Hello World") # No escape characters and printed on single line
+
+print("Hello \nWorld") # \n using to printing in "New Line"
+
+print("City\t: Lisboa") # \t is using to printing a "Tab space"
+print("Country\t: Portugal")
+
+print("Hello \"K1\"") # \ is using the start and the before end of double quotations to print " " marks
+
+print("Hello 'K2'") # Can print single quotations ' ' without any issue 
+
+print("This is \\n not a new line") # If put \\ we can print the \
+
